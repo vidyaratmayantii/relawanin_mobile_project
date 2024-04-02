9495f39f9228404258b02f299202c64787b99d78
Vanillateaa <mohamad.william24@gmail.com>
2024-04-02 21:25:18 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const carikegiatan(),
+        '/': (context) => const LoginPage(),
         '/dashboard': (context) => const DashboardPage(),
       },
     );
